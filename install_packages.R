916cd69faf739a9a7252672090eae6573c0d7a07
Paul Oldham <poldham@mac.com>
2022-03-11 12:22:27 +0000

    add git steps

---
+++ install_packages.R
@@ -0,0 +1,16 @@
+# packages that are needed to maintain the site
+# please add any new packages to this list
+# contributors will need to install these packahes
+# in order to build the distill site 
+
+install.packages("distill")
+install.packages("arrow")
+install.packages("tidyverse")
+install.packages("usethis")
+install.packages("tidytext")
+install.packages("data.table")
+install.packages("gt")
+install.packages("glue")
+install.packages("vroom")
+install.packages("janitor")
+install.packages("rvest")
